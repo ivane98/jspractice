f0d626184588707a6fb8c05765219d6b21af40a2
ivane98 <i.vardoshvili222gmail.com>
2023-02-27 14:15:50 +0400

    ladder

--- index.js
+++ index.js
@@ -1,54 +1,16 @@
-// function isEmpty(obj) {
-//   for (let key in obj) {
-//     // if the loop has started, there is a property
-//     return false;
-//   }
-//   return true;
-// }
-
-// let schedule = {};
-
-// alert(isEmpty(schedule)); // true
-
-// schedule["8:30"] = "get up";
-
-// alert(isEmpty(schedule)); //false
-
-// let salaries = {
-//   John: 100,
-//   Ann: 160,
-//   Pete: 130,
-// };
-
-// let sum = 0;
-
-// for (p in salaries) {
-//   sum += salaries[p];
-// }
-
-// console.log(sum);
-
-let menu = {
-  width: 200,
-  height: 300,
-  title: "My menu",
+let ladder = {
+  step: 0,
+  up() {
+    this.step++;
+    return this;
+  },
+  down() {
+    this.step--;
+    return this;
+  },
+  showStep: function () {
+    // shows the current step
+    alert(this.step);
+    return this;
+  },
 };
-
-function multiplyNumeric(obj) {
-  for (let key in obj) {
-    if (typeof obj[key] == "number") {
-      obj[key] *= 2;
-    }
-  }
-}
-
-multiplyNumeric(menu);
-
-console.log(menu);
-
-// after the call
-// menu = {
-//   width: 400,
-//   height: 600,
-//   title: "My menu",
-// };
