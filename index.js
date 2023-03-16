65fd5fd8eb4525a4757abcceee8e6aa93e40887e
ivane98 <i.vardoshvili22@gmail.com>
2023-03-16 10:30:45 +0400

    leetcode

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
-let ladder = {
-  step: 0,
-  up() {
-    this.step++;
-    return this;
-  },
-  down() {
-    this.step--;
-    return this;
-  },
-  showStep: function () {
-    // shows the current step
-    alert(this.step);
-    return this;
-  },
-};
+//66. Plus One
+
+// var plusOne = function (digits) {
+//   const fromArray = digits.join("");
+//   const toInt = parseInt(fromArray);
+//   const addOne = toInt + 1;
+//   const result = Array.from(String(addOne), Number);
+//   return result;
+// };
+
+// console.log(plusOne([1, 2, 3]));
